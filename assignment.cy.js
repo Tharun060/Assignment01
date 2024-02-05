a7c12b79caa76c551cf6ba9c79b4a8f251cc8b6b
Tharun060 <159001170+Tharun060@users.noreply.github.com>
2024-02-05 13:28:15 +0530

    Add files via upload

---
+++ assignment.cy.js
@@ -0,0 +1,35 @@
+describe('Home', ()=>{
+
+    it('page', ()=>{
+        cy.visit("https://testpages.herokuapp.com/styled/tag/dynamic-table.html")
+
+        cy.get("summary").should('be.visible').click()
+
+        cy.get("#jsondata").clear()
+
+        const jsonData=[
+                        {"name" : "Bob", "age" : 20, "gender": "male"}, 
+                        {"name": "George", "age" : 42, "gender": "male"},
+                        {"name": "Sara", "age" : 42, "gender": "female"}, 
+                        {"name": "Conor", "age" : 40, "gender": "male"}, 
+                        {"name": "Jennifer", "age" : 42, "gender": "female"}]
+
+        cy.get("#jsondata").type(JSON.stringify(jsonData), {parseSpecialCharSequences: false })
+
+        cy.get("#refreshtable").click()
+
+        cy.get("#dynamictable tr:nth-child(n+3)").then(($rows)=>{
+            expect($rows).to.have.length(jsonData.length)
+
+             cy.get("#dynamictable tr:nth-child(n+3)").each(($row, index)=>{
+                const actualData=$row.find('td').map((_, cell)=> Cypress.$(cell).text())
+                const expectedData = Object.values(jsonData[index]).map(jsonData)
+                expect(actualData).to.equal(expectedData)
+             })
+
+             ;
+        })
+
+
+    })
+})
